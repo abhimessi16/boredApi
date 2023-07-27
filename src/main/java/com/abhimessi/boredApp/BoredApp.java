097b28dad99526d7c580da9a91385a098c41d4b6
abhimessi16 <piabhi18@gmail.com>
2023-07-28 00:21:58 +0530

    added application start point

--- src/main/java/com/abhimessi/boredApp/BoredApp.java
+++ src/main/java/com/abhimessi/boredApp/BoredApp.java
@@ -1,8 +1,16 @@
 package com.abhimessi.boredApp;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class BoredApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ApplicationContext applicationContext = SpringApplication.run(BoredApp.class, args);
+
     }
 }
